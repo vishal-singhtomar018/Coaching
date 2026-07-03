784de3948dd0571dd353f44892ccbc6a10ba0e34
vishal singh tomar <vishaltomar731@gmail.com>
2026-07-04 01:54:13 +0530

    updated

--- routes/mentorRoutes.js
+++ routes/mentorRoutes.js
@@ -3,15 +3,17 @@ const router = express.Router();
 
 const mentorController = require("../controllers/mentorController");
 const { upload } = require("../utils/cloudinary");
+const { isLoggedIn } = require("../middleware/authMiddleware");
+const { isAdmin } = require("../middleware/adminMiddleware");
 
 // All mentors
 router.get("/mentors", mentorController.mentorsPage);
 
-router.get("/mentors/:id", mentorController.mentorDetails);
+router.get("/mentors/:id",isLoggedIn, isAdmin, mentorController.mentorDetails);
 
 router.get("/join-tutor", mentorController.joinTutorPage);
 
-router.get("/admin/mentors/add", mentorController.addMentorPage);
+router.get("/admin/mentors/add",isLoggedIn, isAdmin, mentorController.addMentorPage);
 
 router.post(
   "/admin/mentors/add",
